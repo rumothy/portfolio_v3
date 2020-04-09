3550542bc1b2f767cd4235e42735158d9718e278
rumothy <rumothy@yahoo.com>
2020-04-09 15:52:00 -0400

    Add react bootstrap for nav and jumbotron

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MyNav from "./components/MyNav";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyNav />
+      <Jumbotron fluid className="App-header">
+        <h1>HI, I'M ROSS</h1>
+        <p>Software Engineer and UX Artist</p>
+      </Jumbotron>
     </div>
   );
 }
---
+++ src/components/MyNav.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Nav from "react-bootstrap/Nav";
+
+class MyNav extends Component {
+  render() {
+    return (
+      <Nav defaultActiveKey="/home" as="ul">
+        <Nav.Item as="li">
+          <Nav.Link style={{ color: "#0f4c81", height: "32px" }} href="/home">
+            Ross Agginie
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="aboutLink">About</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="experiencesLink">Experience</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="servicesLink">Services</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="portfolioLink">Portfolio</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="contactLink">Contact</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    );
+  }
+}
+
+export default MyNav;
