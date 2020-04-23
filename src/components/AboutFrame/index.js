76c8677ad20c2fef843229c36ca70ac86c537f89
rumothy <rumothy@yahoo.com>
2020-04-23 07:01:11 -0400

    AboutFrame first iteration.

--- src/components/AboutFrame/index.js
+++ src/components/AboutFrame/index.js
@@ -1,13 +1,33 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 const AboutFrame = () => {
   return (
-    <Container
-      className="block-example border border-dark"
-      style={{ minHeight: "65vh" }}
-    >
-      About Me
+    <Container className="" style={{ minHeight: "65vh" }}>
+      <Row>
+        <Col style={{ color: "#0f4c81", marginTop: "150px" }}>
+          <h2>A Little About Me</h2>
+          <p>
+            Hey! My name is Ross. I'm an experienced developer with a passion
+            for front end design. My principles are continuous learning through
+            the creation of real projects.
+          </p>
+          <p>
+            When I'm not coding, I enjoy making music, writing, and being
+            outside.
+          </p>
+        </Col>
+        <Col>
+          <Image
+            src="ross_proffessional.jpg"
+            roundedCircle
+            style={{ width: "412px", height: "424px", margin: "50px" }}
+          />
+        </Col>
+      </Row>
     </Container>
   );
 };
