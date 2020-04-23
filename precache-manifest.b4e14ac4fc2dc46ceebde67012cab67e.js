eb45ded9c50cb8a7c02293475c00e7feed8782cb
rumothy <rumothy@yahoo.com>
2020-04-23 07:48:49 -0400

    Updates

---
+++ precache-manifest.b4e14ac4fc2dc46ceebde67012cab67e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51ed4d2add5c91e127f2d3bfd7d4538",
+    "url": "/portfolio_v3/index.html"
+  },
+  {
+    "revision": "713b8bbc1b9b8afb4752",
+    "url": "/portfolio_v3/static/css/main.972272fa.chunk.css"
+  },
+  {
+    "revision": "add7addb7d199bf76438",
+    "url": "/portfolio_v3/static/js/2.2c38d495.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio_v3/static/js/2.2c38d495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713b8bbc1b9b8afb4752",
+    "url": "/portfolio_v3/static/js/main.89213a05.chunk.js"
+  },
+  {
+    "revision": "f2dd7f98f6124fedae63",
+    "url": "/portfolio_v3/static/js/runtime-main.747c4bc3.js"
+  }
+]);
