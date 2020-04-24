ddbdcb315d042b7c1d5c13fffb4fb72b5216f080
rumothy <rumothy@yahoo.com>
2020-04-24 01:41:51 -0400

    Add resume. Add benchmark pic.

--- src/components/ExperienceFrame/index.js
+++ src/components/ExperienceFrame/index.js
@@ -1,7 +1,29 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const ExperienceFrame = () => {
+  const expItems = [
+    {
+      Id: 1,
+      Company: "Rumothy LLC",
+      Position: "Software Developer & Mentor",
+      City: "Orlando",
+      From: "August 2019",
+      To: "Present",
+      Description: "",
+    },
+    {
+      Id: 2,
+      Company: "Benchmark Electronics",
+      Position: "Software Engineer",
+      City: "Orlando",
+      From: "November 2014",
+      To: "July 2019",
+      Description: "",
+    },
+  ];
   return (
     <Container
       fluid
@@ -10,7 +32,13 @@ const ExperienceFrame = () => {
         background: "#F5F5F5",
       }}
     >
-      Experience
+      <h2>Experience</h2>
+      {expItems.map((expItem) => (
+        <Row key={expItem.Id}>
+          <Col>{expItem.Company}</Col>
+          <Col>{expItem.Position}</Col>
+        </Row>
+      ))}
     </Container>
   );
 };
