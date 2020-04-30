b1ee09a256fd702e7f9470618f884932c89536d7
rumothy <rumothy@yahoo.com>
2020-04-30 17:22:42 -0400

    Move experiences to json file.

--- src/components/ExperienceFrame/index.js
+++ src/components/ExperienceFrame/index.js
@@ -2,45 +2,55 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+//src={`${process.env.PUBLIC_URL}/logo192.png`}
 
-const ExperienceFrame = () => {
-  const expItems = [
-    {
-      Id: 1,
-      Company: "Rumothy LLC",
-      Position: "Software Developer & Mentor",
-      City: "Orlando",
-      From: "August 2019",
-      To: "Present",
-      Description: "",
-    },
-    {
-      Id: 2,
-      Company: "Benchmark Electronics",
-      Position: "Software Engineer",
-      City: "Orlando",
-      From: "November 2014",
-      To: "July 2019",
-      Description: "",
-    },
-  ];
+//src="https://placehold.it/200x200?text=SecondText"
+function ExperienceFrame(props) {
+  const experiences = props.experiences;
   return (
     <Container
       fluid
       style={{
-        minHeight: "65vh",
+        minHeight: "50vh",
         background: "#F5F5F5",
       }}
     >
       <h2>Experience</h2>
-      {expItems.map((expItem) => (
-        <Row key={expItem.Id}>
-          <Col>{expItem.Company}</Col>
-          <Col>{expItem.Position}</Col>
-        </Row>
+      {experiences.map((experience) => (
+        <Card key={experience.Id} border="dark" style={{ width: "100%" }}>
+          <Card.Header>{experience.Company}</Card.Header>
+          <Card.Body>
+            <Card.Title>{experience.Position}</Card.Title>
+            <Card.Text>{experience.Description}</Card.Text>
+          </Card.Body>
+        </Card>
       ))}
+      <br />
+      {/* <Card border="dark" style={{ width: "100%" }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Dark Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+      <Card border="dark" style={{ width: "100%" }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Dark Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br /> */}
     </Container>
   );
-};
+}
 
 export default ExperienceFrame;
