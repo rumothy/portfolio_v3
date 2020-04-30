4393bd9c0adfe44ee77bbc0d008c2301699888c3
rumothy <rumothy@yahoo.com>
2020-04-30 18:51:48 -0400

    Use icon grid for experiences.

--- src/components/ExperienceFrame/index.js
+++ src/components/ExperienceFrame/index.js
@@ -2,53 +2,60 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
 //src={`${process.env.PUBLIC_URL}/logo192.png`}
 
 //src="https://placehold.it/200x200?text=SecondText"
+
+function getImages() {
+  let images = [];
+  for (let i = 0; i < 18; i++) {
+    images.push(`https://placehold.it/127x127?text=Icon ${i + 1}`);
+  }
+  return images;
+}
+
 function ExperienceFrame(props) {
-  const experiences = props.experiences;
+  const images = getImages();
   return (
     <Container
       fluid
       style={{
         minHeight: "50vh",
         background: "#F5F5F5",
+        color: "#0f4c81",
       }}
     >
       <h2>Experience</h2>
-      {experiences.map((experience) => (
-        <Card key={experience.Id} border="dark" style={{ width: "100%" }}>
-          <Card.Header>{experience.Company}</Card.Header>
-          <Card.Body>
-            <Card.Title>{experience.Position}</Card.Title>
-            <Card.Text>{experience.Description}</Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
-      <br />
-      {/* <Card border="dark" style={{ width: "100%" }}>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-          <Card.Title>Dark Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br />
-      <Card border="dark" style={{ width: "100%" }}>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-          <Card.Title>Dark Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br /> */}
+      <Row style={{ textAlign: "center" }}>
+        {" "}
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row>
+        {images.slice(0, 6).map((image) => (
+          <Col key={image} style={{ padding: "0px 15px 15px" }}>
+            <Image src={image} />
+          </Col>
+        ))}
+      </Row>
+
+      <Row>
+        {images.slice(6, 12).map((image) => (
+          <Col key={image} style={{ padding: "0px 15px 15px" }}>
+            <Image src={image} />
+          </Col>
+        ))}
+      </Row>
+
+      <Row>
+        {images.slice(12, 18).map((image) => (
+          <Col key={image} style={{ padding: "0px 15px 15px" }}>
+            <Image src={image} />
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 }
